ca0762557060c2e1703f612514d9e6c8c994bd2c
Victoria Oberascher <S1810824017@students.fh-hagenberg.at>
2019-05-23 14:44:36 +0200

    lines added

--- ciesla_oberascher_android/MyProject/main.cpp
+++ ciesla_oberascher_android/MyProject/main.cpp
@@ -9,4 +9,9 @@ public void thisIsANewMethod() {
     printf("this is a new method");
 	std::cout << "Hallo";
     printf("different line of code from member2");
+    
+    printf("line 1of code from member2");
+    
+    printf("line of code 2 from member2");
+    printf("line of code 3 from member2");
 }
